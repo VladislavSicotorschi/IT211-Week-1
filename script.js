1b0f4aa8d9b173d78b1521c2368e9f1de66dc406
Vladislav Sicotorschi <vvladic123@gmail.com>
2023-06-28 14:39:10 -0700

    y

---
+++ script.js
@@ -0,0 +1,6 @@
+//firstName, lastName, yearsOfStudy, and goal.
+
+let firstName = Vladislav;
+let lastName = Sicotorschi;
+let yearsOfStudy = 3 months;
+let goal = "Learn as much as i can and apply that knowledge";
