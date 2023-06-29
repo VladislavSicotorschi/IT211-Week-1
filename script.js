edb670ca36b39b52223c82d73fd237c2d571690b
Vladislav Sicotorschi <vvladic123@gmail.com>
2023-06-28 23:18:31 -0700

    y

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ const yearsOfStudy = "3 months";
 const goal = "learn as much as i can, aswell as apply that knowledge";
 
 let myDiv = document.getElementById("aboutMe");
-myDiv.innerHTML = "My Name is " + firstName +" "+ lastName + ". I have been at SCC for " + yearsOfStudy + " and i plan to "+ goal + "after completing my courses.";
+myDiv.innerHTML = "My Name is " + firstName +" "+ lastName + ". I have been at SCC for " + yearsOfStudy + " and i plan to "+ goal + " after completing my courses.";
